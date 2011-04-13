de403b10fac5522f7d42406ba79cd5148361769f
olegivo@gmail.com <vovano2006@gmail.com>
2011-04-13 01:58:04 +0000

    Добавлен теплообменник

---
+++ TP/TP/HeatExchanger/ucAllHeatExchanger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace HeatExchanger
+{
+    public partial class ucAllHeatExchanger : DevExpress.XtraEditors.XtraUserControl
+    {
+        public ucAllHeatExchanger()
+        {
+            InitializeComponent();
+        }
+    }
+}
