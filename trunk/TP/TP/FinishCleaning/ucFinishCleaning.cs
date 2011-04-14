bfcfb437e9886e0a5d4bdd25c4652af31804305a
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2011-04-14 17:05:06 +0000

    Добавлены недостающие ЭУ для финишной очистки

---
+++ trunk/TP/TP/FinishCleaning/ucFinishCleaning.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TP.FinishCleaning
+{
+    public partial class ucFinishCleaning : DevExpress.XtraEditors.XtraUserControl
+    {
+        public ucFinishCleaning()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void ucFinishCleaning_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
