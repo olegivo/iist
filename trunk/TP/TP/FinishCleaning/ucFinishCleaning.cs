70e747ed0d07fcff240f90f18a94d7c550fe9661
artemjke74 <artemjke74@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2011-04-20 15:55:33 +0000

    Датчики с диапазонами и границами аварий

--- trunk/TP/TP/FinishCleaning/ucFinishCleaning.cs
+++ trunk/TP/TP/FinishCleaning/ucFinishCleaning.cs
@@ -9,6 +9,16 @@ namespace TP.FinishCleaning
             InitializeComponent();
         }
 
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden), Browsable(false)]
+        //public float Level3
+        //{
+        //    get { return ucTube.Leve3; }
+        //    set { ucGauge1.EditValue = ucTube.Leve3 = value; }
+        //}
+
       
 
         private void ucFinishCleaning_Load(object sender, EventArgs e)
