6bd27561de69eb63f3c27b6e99c9127fd2a185cd
Dmitrieva.Irina.S@gmail.com <Dmitrieva.Irina.S@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2011-05-04 14:13:43 +0000

    Прописала каналы дл теплообменника Исправила названия с Level_TP4 на Temperature_TP4

--- trunk/TP/TP/HeatExchanger/ucAllHeatExchanger.cs
+++ trunk/TP/TP/HeatExchanger/ucAllHeatExchanger.cs
@@ -11,25 +11,25 @@ namespace TP.HeatExchanger
         }
 
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden), Browsable(false)]
-        private float Level_O2
+        public float Concentration_O2
         {
             get { return ucIndicator1.EditValue; }
             set { ucIndicator1.EditValue = value; }
         }
 
-        private float Level_CO
+        public float Concentration_CO
         {
             get { return ucIndicator2.EditValue; }
             set { ucIndicator2.EditValue = value; }
         }
 
-        private float Level_TP4
+        public float Temperature_TP4
         {
             get { return ucIndicator3.EditValue; }
             set { ucIndicator3.EditValue = value; }
         }
 
-        private float Level_TP5
+        public float Temperature_TP5
         {
             get { return ucIndicator4.EditValue; }
             set { ucIndicator4.EditValue = value; }
@@ -37,22 +37,22 @@ namespace TP.HeatExchanger
 
         private void spinEdit1_EditValueChanged(object sender, System.EventArgs e)
         {
-            Level_O2 = (float)Convert.ToDecimal(spinEdit1.EditValue);
+            Concentration_O2 = (float)Convert.ToDecimal(spinEdit1.EditValue);
         }
 
         private void spinEdit2_EditValueChanged(object sender, EventArgs e)
         {
-            Level_CO = (float)Convert.ToDecimal(spinEdit2.EditValue);
+            Concentration_CO = (float)Convert.ToDecimal(spinEdit2.EditValue);
         }
 
         private void spinEdit3_EditValueChanged(object sender, EventArgs e)
         {
-            Level_TP4 = (float)Convert.ToDecimal(spinEdit3.EditValue);
+            Temperature_TP4 = (float)Convert.ToDecimal(spinEdit3.EditValue);
         }
 
         private void spinEdit4_EditValueChanged(object sender, EventArgs e)
         {
-            Level_TP5 = (float)Convert.ToDecimal(spinEdit4.EditValue);
+            Temperature_TP5 = (float)Convert.ToDecimal(spinEdit4.EditValue);
         }
     }
 }
