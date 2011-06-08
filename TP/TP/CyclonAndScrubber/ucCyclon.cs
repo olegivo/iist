2eea8a27487755fa0bfa9dc765a5836898d51472
olegivo@gmail.com <vovano2006@gmail.com>
2011-06-08 03:29:36 +0000

    Циклон

--- TP/TP/CyclonAndScrubber/ucCyclon.cs
+++ TP/TP/CyclonAndScrubber/ucCyclon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 using UICommon;
 
@@ -16,7 +17,19 @@ namespace TP.CyclonAndScrubber
         public ucCyclon()
         {
             InitializeComponent();
+
+            myBrush1 = Brushes.Gray;
+            myBrush2 = new LinearGradientBrush(new Point(0, YMax), new Point(GradWidth, YMax), Color.DarkGray, Color.Gray);
+            myBrush3 = new LinearGradientBrush(new Point(XMax - GradWidth - 1, YMax), new Point(XMax, YMax), Color.Gray, Color.DimGray);
+            pen = new Pen(LookAndFeel.Painter.Border.DefaultAppearance.ForeColor);
+
         }
+
+        private readonly Brush myBrush1;
+        private readonly LinearGradientBrush myBrush2;
+        private readonly LinearGradientBrush myBrush3;
+        private readonly Pen pen;
+        private const int GradWidth = 10;
 
         /// <summary>
         /// 
@@ -24,23 +37,23 @@ namespace TP.CyclonAndScrubber
         /// <param name="e"></param>
         protected override void OnPaint(PaintEventArgs e)
         {
-            // Create a local version of the graphics object for the PictureBox.
             Graphics g = e.Graphics;
-            int xMax = Width - 1;
-            int yMax = Height - 1;
             int rem;
-            int xCenter = Math.DivRem(xMax, 2, out rem);
-            //int yCenter = Math.DivRem(yMax, 2, out rem);
-            int yShift = Math.DivRem(xMax, 2, out rem);
+            int yShift = Math.DivRem(YMax, 2, out rem);
 
-            // Draw a line in the PictureBox.
-            Pen pen = new Pen(LookAndFeel.Painter.Border.DefaultAppearance.ForeColor);// Pens.Black;
-            g.DrawRectangle(pen, 0, 0, xMax, yMax - yShift);
-            //g.DrawLine(pen, 0, 0, xMax, 0);
-            //g.DrawLine(pen, 0, 0, 0, yMax - yShift);
-            //g.DrawLine(pen, xMax, 0, xMax, yMax - yShift);
-            g.DrawLine(pen, 0, yMax - yShift, xCenter, yMax);
-            g.DrawLine(pen, xMax, yMax - yShift, xCenter, yMax);
+            g.FillRectangle(myBrush1, 0, 0, YMax, YMax - yShift);
+            g.FillRectangle(myBrush2, 0, 0, GradWidth, YMax - yShift);
+            g.FillRectangle(myBrush3, YMax - GradWidth, YMax - yShift, YMax, YMax - yShift);
+            g.DrawRectangle(pen, 0, 0, YMax, YMax - yShift);
+
+            Point[] points = new[]
+                                 {
+                                     new Point(0, YMax - yShift),
+                                     new Point(XCenter, YMax),
+                                     new Point(XMax, YMax - yShift),
+                                 };
+            g.FillPolygon(myBrush1, points);
+            g.DrawPolygon(pen, points);
 
             base.OnPaint(e);
         }
