7505293f02ceaf8e01aa97b8b32a672fdb25bf19
artemjke74 <artemjke74@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2011-06-15 18:37:30 +0000

    Эмулятор нижнего уровня: пробы

--- EmulationClient/Emulation/GasConcentration.cs
+++ EmulationClient/Emulation/GasConcentration.cs
@@ -10,37 +10,46 @@ namespace EmulationClient.Emulation
         public double OutputValue
         {
             get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
         }
 
         /// <summary>
         /// Входной параметр: Температура перед рукавным фильтром 
         /// </summary>
         public double Temperature;
+        
+            //if (Burner = true);
+            //{
+            //Temperature = Math.Exp(1/2*DateTime.Now.Second);
+            //}
+            //else          
+            //{
+            //Temperature = Math.Exp(-1/2*DateTime.Now.Second);
+            //}
+        
 
         /// <summary>
         /// Входной параметр: Количеством оборотов дымососа  
         /// </summary>
         public double Speed;
 
+
         /// <summary>
         /// Входной параметр: Горелка Вкл.\Выкл.
         /// </summary>
         public bool Burner;
-
-
-        /// <summary>
-        /// Концентрация выбрасываемого газа СО 
-        /// </summary>
-        public double CCO;
-
+       
         /// <summary>
         /// Обновить значение (используется функция пересчёта входных параметров в выходной)
         /// </summary>
         public void Refresh()
         {
+            DateTime now = DateTime.Now; //todo:зафиксировать время в переменной.
+           int second = now.Second;
+           OutputValue = Math.Abs(Math.Sin(0.005*second))* 500 + 3500 + Temperature + Speed;
+
             throw new NotImplementedException();
         }
 
-        //todo: добавить входные параметры
     }
 }
