41ef82bddfb2d72816a67830723c31822e086a2c
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2011-07-11 06:42:15 +0000

    Добавлен циклон и скруббер

---
+++ branches/BlendTryingTemp/TP/TP/CycloneAndScrubber/ucCycloneAndScrubber.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace TP.CycloneAndScrubber
+{
+	/// <summary>
+	/// Interaction logic for ucCycloneAndScrubber.xaml
+	/// </summary>
+	public partial class ucCycloneAndScrubber : UserControl
+	{
+	    /// <summary>
+	    /// 
+	    /// </summary>
+	    public ucCycloneAndScrubber()
+		{
+			InitializeComponent();
+		}
+	}
+}
