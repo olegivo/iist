22bfefa9f741206af9c20e2abddd1f7744d51fbe
artemjke74 <artemjke74@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2011-10-10 19:22:52 +0000

    пробы с горелкой

--- EmulationClient/Window1.xaml.cs
+++ EmulationClient/Window1.xaml.cs
@@ -26,15 +26,18 @@ namespace EmulationClient
             controlManagementUnitEmulation.RegisterAllChannels();
         }
 
+
         private void radioButton1_Checked(object sender, RoutedEventArgs e)
         {
             // вкл. горелку
-
+            MessageBox.Show("Горелка была включена");
+           // radioButton1.SetValue(true = ));
         }
 
         private void radioButton2_Checked(object sender, RoutedEventArgs e)
         {
             // выкл. горелку
+            MessageBox.Show("Горелка выключена");
         }
     }
 }
