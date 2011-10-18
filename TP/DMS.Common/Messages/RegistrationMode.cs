a567746bdc5c4d7d06d1528e11746878f6a818a8
olegivo@gmail.com <vovano2006@gmail.com>
2011-10-18 03:21:33 +0000

    Добавлены перечисления для режимов: работы с каналом, регистрации, подписки Добавлено сообщение для регистрации/отмены регистрации канала TODO: внедрить вышеописанное

---
+++ TP/DMS.Common/Messages/RegistrationMode.cs
@@ -0,0 +1,18 @@
+﻿namespace DMS.Common.Messages
+{
+    /// <summary>
+    /// Режим регистрации
+    /// </summary>
+    public enum RegistrationMode
+    {
+        /// <summary>
+        /// Регистрация
+        /// </summary>
+        Register = 1,
+
+        /// <summary>
+        /// Отмена регистрации
+        /// </summary>
+        Unregister = 2
+    }
+}
