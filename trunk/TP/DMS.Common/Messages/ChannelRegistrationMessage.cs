a01e7b752f73b38366b0bcda782c959982edab08
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2011-10-18 03:21:33 +0000

    Добавлены перечисления для режимов: работы с каналом, регистрации, подписки Добавлено сообщение для регистрации/отмены регистрации канала TODO: внедрить вышеописанное

---
+++ trunk/TP/DMS.Common/Messages/ChannelRegistrationMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace DMS.Common.Messages
+{
+    /// <summary>
+    /// Сообщение для регистрации/отмены регистрации канала
+    /// </summary>
+    public class ChannelRegistrationMessage : RegistrationMessage
+    {
+        /// <summary>
+        /// Номер канала
+        /// </summary>
+        public int LogicalChannelId { get; set; }
+
+        /// <summary>
+        /// Режим работы с каналом
+        /// </summary>
+        public ChannelMode ChannelMode { get; set; }
+    }
+}
