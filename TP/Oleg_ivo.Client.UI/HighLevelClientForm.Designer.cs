b87b850f33a4a822cdd3e363ace136faffe4d1f3
olegivo@gmail.com <vovano2006@gmail.com>
2012-05-09 12:51:08 +0000

    UI Клиент верхнего уровня: диалог для ввода значения для записи в канал

--- TP/Oleg_ivo.Client.UI/HighLevelClientForm.Designer.cs
+++ TP/Oleg_ivo.Client.UI/HighLevelClientForm.Designer.cs
@@ -36,6 +36,7 @@ namespace Oleg_ivo.HighLevelClient.UI
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnSendError = new System.Windows.Forms.Button();
             this.doubleListBoxControl1 = new Oleg_ivo.Tools.UI.DoubleListBoxControl();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -99,6 +100,7 @@ namespace Oleg_ivo.HighLevelClient.UI
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.btnSendError);
             this.groupBox1.Controls.Add(this.doubleListBoxControl1);
             this.groupBox1.Controls.Add(this.btnRegister);
@@ -130,6 +132,17 @@ namespace Oleg_ivo.HighLevelClient.UI
             this.doubleListBoxControl1.TabIndex = 7;
             this.doubleListBoxControl1.ItemMoved += new System.EventHandler<Oleg_ivo.Tools.UI.MovedEventArgs>(this.doubleListBoxControl1_ItemMoved);
             // 
+            // button1
+            // 
+            //this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(6, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Write channel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSendError_Click);
+            // 
             // HighLevelClientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -158,6 +171,7 @@ namespace Oleg_ivo.HighLevelClient.UI
         private System.Windows.Forms.GroupBox groupBox1;
         private Oleg_ivo.Tools.UI.DoubleListBoxControl doubleListBoxControl1;
         private System.Windows.Forms.Button btnSendError;
+        private System.Windows.Forms.Button button1;
     }
 }
 
