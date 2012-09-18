ff8110690aa8a68b297c84181ba743e09b08e184
vovano2006@gmail.com <vovano2006@gmail.com>
2012-09-18 15:37:04 +0000

    Создана модель представления для: -Барабанная вращающаяся печь -Камера дожигания -Теплообменник -Циклон и скруббер

---
+++ TP/TP.WPF/ViewModels/AllHeatExchangerViewModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JulMar.Windows.Mvvm;
+
+namespace TP.WPF.ViewModels 
+{
+    public class AllHeatExchangerViewModel: ViewModel
+    {
+        /// <summary>
+        /// TП3	температура в камере дожигания
+        /// </summary>
+        private float _temperature3;
+        public float Temperature_TP3
+        {
+            get { return _temperature3; }
+            set
+            {
+                if (_temperature3 != value)
+                {
+                    _temperature3 = value;
+                    OnPropertyChanged("Temperature_TP3");
+                }
+            }
+        }
+        /// <summary>
+        /// TР4	температура в теплообменнике ТО1
+        /// </summary>
+        private float _temperature4;
+        public float Temperature_TR4
+        {
+            get { return _temperature4; }
+            set
+            {
+                if (_temperature4 != value)
+                {
+                    _temperature4 = value;
+                    OnPropertyChanged("Temperature_TR4");
+                }
+            }
+        }
+        /// <summary>
+        /// TР5	температура в теплообменнике ТО2
+        /// </summary>
+        private float _temperature5;
+        public float Temperature_TR5
+        {
+            get { return _temperature5; }
+            set
+            {
+                if (_temperature5 != value)
+                {
+                    _temperature5 = value;
+                    OnPropertyChanged("Temperature_TR5");
+                }
+            }
+        }
+        /// <summary>
+        /// Г-О2	концентрация газа О2
+        /// </summary>
+        private float _concentration_O2;
+        public float GasConcentration_O2
+        {
+            get { return _concentration_O2; }
+            set
+            {
+                if (_concentration_O2 != value)
+                {
+                    _concentration_O2 = value;
+                    OnPropertyChanged("GasConcentration_O2");
+                }
+            }
+        }
+        /// <summary>
+        /// Г-СО	концентрация газа СО
+        /// </summary>
+        private float _concentration_CO;
+        public float GasConcentration_CO
+        {
+            get { return _concentration_CO; }
+            set
+            {
+                if (_concentration_CO != value)
+                {
+                    _concentration_CO = value;
+                    OnPropertyChanged("GasConcentration_CO");
+                }
+            }
+        }
+    }
+}
---
+++ TP/TP.WPF/ViewModels/CycloneAndScrubberViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JulMar.Windows.Mvvm;
+
+namespace TP.WPF.ViewModels
+{
+    public class CycloneAndScrubberViewModel : ViewModel
+    {
+        /// <summary>
+        /// рН1	уровень рН в СФ1
+        /// </summary>
+        private float _phLevel1;
+        public float PhLevel_CF1
+        {
+            get { return _phLevel1; }
+            set
+            {
+                if (_phLevel1 != value)
+                {
+                    _phLevel1 = value;
+                    OnPropertyChanged("PhLevel_CF1");
+                }
+            }
+        }
+
+        /// <summary>
+        /// рН2	уровень рН в СФ2
+        /// </summary>
+        private float _phLevel2;
+        public float PhLevel_CF2
+        {
+            get { return _phLevel2; }
+            set
+            {
+                if (_phLevel2 != value)
+                {
+                    _phLevel2 = value;
+                    OnPropertyChanged("PhLevel_CF2");
+                }
+            }
+        }
+        /// <summary>
+        /// ДУ-10	уровень в СБ
+        /// </summary>
+        private float _level10;
+        public float Level_DU10
+        {
+            get { return _level10; }
+            set
+            {
+                if (_level10 != value)
+                {
+                    _level10 = value;
+                    OnPropertyChanged("Level_DU10");
+                }
+            }
+        }
+    }
+}
