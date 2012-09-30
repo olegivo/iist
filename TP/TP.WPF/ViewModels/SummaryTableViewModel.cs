0e2ddc9918a74f33e7569b006e8dd148d01eca6e
vovano2006@yandex.ru <vovano2006@yandex.ru>
2012-09-30 18:36:27 +0000

    Добавлена дополнительная вкладка "Сводная таблица"

---
+++ TP/TP.WPF/ViewModels/SummaryTableViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using JulMar.Windows.Mvvm;
+using System.Linq;
+using System.Text;
+
+namespace TP.WPF.ViewModels
+{
+
+    public class SummaryTableViewModel
+    {
+        private Dictionary<int, float> _summaryTable = new Dictionary<int, float>();
+        
+        public void Add(int chanalNumber, float chanelValue)
+        {
+            _summaryTable.Add(chanalNumber,chanelValue);
+        }
+ 
+    
+    }
+
+}
