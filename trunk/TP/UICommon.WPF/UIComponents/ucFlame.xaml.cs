d3d5132f85382c46951094dfbdd2f75527c77cfc
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2012-10-07 15:57:26 +0000

    Изменены неймспейсы для общих контролов

--- trunk/TP/UICommon.WPF/UIComponents/ucFlame.xaml.cs
+++ trunk/TP/UICommon.WPF/UIComponents/ucFlame.xaml.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace UICommonWPF
+namespace UICommon.WPF.UIComponents
 {
 	/// <summary>
-	/// Interaction logic for UserControl1.xaml
+	/// Interaction logic for ucFlame.xaml
 	/// </summary>
-	public partial class UserControl1 : UserControl
+	public partial class ucFlame : UserControl
 	{
-		public UserControl1()
+		public ucFlame()
 		{
 			this.InitializeComponent();
 		}
