0d33e690513384615dbfa1d1cc29a8de76213d4a
olegivo@gmail.com <vovano2006@gmail.com>
2012-10-10 21:00:03 +0000

    Добавлен MVVM-индикатор Добавлен ObserverableDictionary Упрощена логика настройки индикаторов (теперь настраиваются их VM) Упрощена логика обновления значений (теперь их получают их VM)

---
+++ TP/UICommon.WPF/UIComponents/Indicator.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace UICommon.WPF.UIComponents
+{
+    /// <summary>
+    /// Interaction logic for ucIndicator.xaml
+    /// </summary>
+    public partial class Indicator : UserControl
+    {
+        public Indicator()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
