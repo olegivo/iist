e73961dcb9fff372626fae499eccadc5ef1acd39
vovavo2006@gmail.com <vovavo2006@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2013-03-24 19:17:53 +0000

    Добавлены и подключены пробные образцы Стрелочных индикаторов, целиком использующих ViewModels/IndicatorViewModel.cs Удалена директория UIComponents (из-за ненадобности содержимого) добавлена дирректория UIIndicators Indicator.xaml перемещен и теперь храниться под именем ucSimpleIndicator

---
+++ branches/VS2010/TP/UICommon.WPF/UIIndicators/ucSimpleIndicator.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows.Controls;
+
+namespace UICommon.WPF.UIIndicators
+{
+    /// <summary>
+    /// Interaction logic for ucIndicator.xaml
+    /// </summary>
+    public partial class ucSimpleIndicator : UserControl
+    {
+        public ucSimpleIndicator()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
