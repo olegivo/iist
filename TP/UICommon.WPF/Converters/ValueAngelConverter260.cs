4490cd21b49c7bbccee0b3b6c8a014519fb374ad
vovavo2006@gmail.com <vovano2006@gmail.com>
2013-06-17 19:48:59 +0000

    Доработан дизайн печи - добавлена анимация и обновлены индикаторы (от Игоря) Обноввлен ucLevelIndicator, исправлен конвертер

---
+++ TP/UICommon.WPF/Converters/ValueAngelConverter260.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UICommon.WPF.Converters
+{
+
+        public class ValueAngleConverter260 : IMultiValueConverter
+        {
+            public object Convert(object[] values, Type targetType, object parameter,
+                System.Globalization.CultureInfo culture)
+            {
+                double? total = 0;
+				double? currentValue = values[0] as double?;
+				double? minValue = values[1] as double?;
+				double? maxValue = values[2] as double?;
+				
+                if (values[0]!= null)
+                {
+					total = (currentValue-minValue)*260/(maxValue-minValue);
+                }
+
+                return total;
+            }
+
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter,
+                System.Globalization.CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+}
