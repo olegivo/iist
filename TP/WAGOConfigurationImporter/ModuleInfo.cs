728805ce4f331f6bf2597c402963ad63dff7798d
vovavo2006@gmail.com <vovano2006@gmail.com>
2013-09-18 07:09:17 +0000

    Добавлен проект WAGOConfigurationImporter для возможности быстрой загрузки конфигурации системы из xml файлов формата  WAGO-I/O-CHECK 2 (проверен на реальном ТП) Issue:

---
+++ TP/WAGOConfigurationImporter/ModuleInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WAGOConfigurationImporter
+{
+    public enum SignalType
+    {
+        Analog,
+        Descrete
+    };
+
+    public enum IOType
+    {
+        Input,
+        Output
+    };
+
+    /// <summary>
+    /// Класс описывающий положение модулей и состав контроллера
+    /// </summary>
+    public class ModuleInfo
+    {
+        public string ModuleName { get; set; }
+        public string ModuleNo { get; set; }
+        public int PhysicalPosition { get; set; }
+        public int ChannelsCount { get; set; }
+        public SignalType? SignalType { get; set; }
+        public IOType? IoType { get; set; }
+    }
+
+    //public class Modules : IEnumerator, IEnumerable
+    //{
+    //    private ModuleInfo[] modules;
+    //    private int position = -1;
+    //    public bool MoveNext()
+    //    {
+    //        position++;
+    //        return (position < modules.Length);
+    //    }
+
+    //    public void Reset()
+    //    {
+    //        position = 0;
+    //    }
+
+    //    public object Current { get { return modules[position]; } }
+    //    public IEnumerator GetEnumerator()
+    //    {
+    //        return (IEnumerator) this;
+    //    }
+
+    //}
+
+}
---
+++ TP/WAGOConfigurationImporter/Visualizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WAGOConfigurationImporter
+{
+    public partial class Visualizer : UserControl
+    {
+        public Visualizer()
+        {
+            InitializeComponent();
+        }
+    }
+}
