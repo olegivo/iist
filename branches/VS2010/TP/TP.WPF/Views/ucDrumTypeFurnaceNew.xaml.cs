10ca0a293528c87c128a359d8d973fccf999773b
vovavo2006@gmail.com <vovavo2006@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2013-11-18 19:30:18 +0000

    Правки Игоря - добавление дискретных индикаторов, изменения в DrumTypeFurnace и прочее

---
+++ branches/VS2010/TP/TP.WPF/Views/ucDrumTypeFurnaceNew.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TP.WPF.Views
+{
+	/// <summary>
+	/// Interaction logic for ucDrumTypeFurnaceNew.xaml
+	/// </summary>
+	public partial class ucDrumTypeFurnaceNew : UserControl
+	{
+		public ucDrumTypeFurnaceNew()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
