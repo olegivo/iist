f9312f5ffcb227d70d8e77458b575be6800ccb8e
vovavo2006@gmail.com <vovavo2006@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2013-12-09 07:01:54 +0000

    Библиотека QuickChartsWPF добавлена в проект (externals использовать не будем)

--- branches/VS2010/TP/TP.WPF/ViewModels/ChartTabViewModel.cs
+++ branches/VS2010/TP/TP.WPF/ViewModels/ChartTabViewModel.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Windows;
 using AmCharts.Windows.QuickCharts;
 using DMS.Common.Messages;
 
@@ -16,21 +15,61 @@ namespace TP.WPF.ViewModels
             
         }
 
-
+        public ObservableCollection<ChartDataItem> ChartBindingData { get { return _data; } }
+        private ObservableCollection<ChartDataItem> _data = new ObservableCollection<ChartDataItem>()
+            {new ChartDataItem()
+                {
+                    ChannelId = 1,
+                    ChannelTime = "2",
+                    ChannelValue = 22
+                },
+            new ChartDataItem()
+                {
+                    ChannelId = 1,
+                    ChannelTime = "3",
+                    ChannelValue = 12
+                    
+                },
+            new ChartDataItem()
+                {
+                    ChannelId = 1,
+                    ChannelTime = "4",
+                    ChannelValue = 15
+                    
+                },
+           new ChartDataItem()
+                {
+                    ChannelId = 1,
+                    ChannelTime = "5",
+                    ChannelValue = 14
+                    
+                }
+            };
         public class ChartDataItem
         {
             public int ChannelId { get; set; }
             public double ChannelValue { get; set; }
             public string ChannelTime { get; set; }
-            public DiscreetClearObservableCollection<SerialGraph> ChannelCharts { get; set; }
         }
 
-        public ObservableCollection<ChartDataItem> ChartBindingData { get { return _data; } }
-        private ObservableCollection<ChartDataItem> _data = new ObservableCollection<ChartDataItem>();
-
-        
-        
-        
+        public DiscreetClearObservableCollection<SerialGraph> _chartCollection = new DiscreetClearObservableCollection
+            <SerialGraph>()
+            {
+                new LineGraph()
+                    {
+                        Title = "TestChart",
+                        ValueMemberPath = "ChannelValue"
+                        
+                        
+                    }
+            };
+        public DiscreetClearObservableCollection<SerialGraph> ChartCollection {
+            set { _chartCollection = value; }
+            get
+            {
+                return _chartCollection;
+            }
+        }
         /// <summary>
         /// После чтения канала
         /// </summary>
@@ -59,11 +98,33 @@ namespace TP.WPF.ViewModels
 
         public override void OnChannelRegistered(ChannelRegistrationMessage message)
         {
-            //base.OnChannelRegistered(message);
-            //ChannelCharts.Add(new LineGraph()
-            //    {
-            //        Title = String.Format("Канал №{0}",message.LogicalChannelId)
-            //    });
+            ChartCollection.Add(new LineGraph()
+                {
+                    Title = "Канал №" + message.LogicalChannelId,
+                    ValueMemberPath = "val" + message.LogicalChannelId,
+                    Name = "graph" + message.LogicalChannelId
+                }
+                );
+        }
+        public override void OnChannelUnRegistered(ChannelRegistrationMessage message)
+        {
+            var gi = SelectSerialGraphItemByChannelId(message.LogicalChannelId);
+            if (gi!=null)
+            {
+                ChartCollection.Remove(gi);
+            }
+        }
+
+        private SerialGraph SelectSerialGraphItemByChannelId(int channelId)
+        {
+            foreach (var serialGraph in ChartCollection)
+            {
+                string gn = "graph" + channelId;
+                if (serialGraph.Name == gn)
+                    return serialGraph;
+
+            }
+            return null;
         }
 
     }
