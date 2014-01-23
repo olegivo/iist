5b239cd7fc82625d702a945df0004bc723102f6e
vovavo2006@gmail.com <vovavo2006@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2014-01-23 19:37:30 +0000

    Выведена первая пробная вкладка ТП на обновленный пользовательский интерфейс

--- branches/VS2010_UI2/TP/TP.WPF/Views/MainShell.xaml.cs
+++ branches/VS2010_UI2/TP/TP.WPF/Views/MainShell.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace TP.WPF
+﻿using System.Windows.Controls;
+using System.Windows.Markup;
+using MixModes.Synergy.VisualFramework.Windows;
+using TP.WPF.Views;
+
+namespace TP.WPF
 {
 	/// <summary>
 	/// Interaction logic for MainShell.xaml
@@ -10,6 +15,23 @@
 			this.InitializeComponent();
 			
 			// Insert code required on object creation below this point.
+		    var v = new Views.MainWindow();
+
+
+
+
+            //Создание лэйаута
+            DockPane _mainPane = new DockPane();
+		    _mainPane.Width = 600;
+		    _mainPane.Height = 400;
+		    _mainPane.MinHeight = 240;
+		    _mainPane.MinWidth = 320;
+		    _mainPane.Header = "главное окно мониторинга";
+		    _mainPane.Content = new Views.ucDrumTypeFurnace();
+            WindowsManager.AddFloatingWindow(_mainPane);
+
+
+		    //UcListView.Items.Add(new Views.ucDrumTypeFurnace());
 		}
 	}
 }
