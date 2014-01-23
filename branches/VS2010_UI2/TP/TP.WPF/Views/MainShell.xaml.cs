2807341ceb13769528c6ce179204f8ddad0c61df
vovavo2006@gmail.com <vovavo2006@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2014-01-23 15:52:13 +0000

    Дозагружен MainShell.xaml для нового интерфейса

---
+++ branches/VS2010_UI2/TP/TP.WPF/Views/MainShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TP.WPF
+{
+	/// <summary>
+	/// Interaction logic for MainShell.xaml
+	/// </summary>
+	public partial class MainShell
+	{
+		public MainShell()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
