4ed92432966051f2a9e46982483da466a82af4e3
vovavo2006@gmail.com <vovavo2006@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2014-01-31 09:33:55 +0000

    Обновление внешнего интерфейса приложения

---
+++ branches/VS2010_UI2/TP/TP.WPF/Resources/ucIndicatorGroup1.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TP.WPF.Resources
+{
+    /// <summary>
+    /// Interaction logic for ucIndicatorGroup1.xaml
+    /// </summary>
+    public partial class ucIndicatorGroup1 : UserControl
+    {
+        //public List<Button> ButItems = new List<Button>()
+        //    {
+        //        new Button(){Name = "bu2",}
+        //    };
+        public ucIndicatorGroup1()
+        {
+            InitializeComponent();
+            //DataList = new List<Button>();
+            //ButItems.Add(new Button() { Name = "but1" });
+        }
+    }
+}
