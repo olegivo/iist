4853a4d8a936a1796b2153de582dfef2ae8d0734
vovavo2006@gmail.com <vovano2006@gmail.com>
2014-02-11 08:00:57 +0000

    Доработки по usercontrol "ветродув"

--- TP/TP.WPF/Views/ucFan.xaml.cs
+++ TP/TP.WPF/Views/ucFan.xaml.cs
@@ -27,13 +27,29 @@ namespace TP.WPF
 
 	    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
 	    {
-	        var ivm = dependencyPropertyChangedEventArgs.NewValue as IndicatorViewModel;
+            //TODO:old value -=
+            var ivm = this.DataContext as IndicatorViewModel;
+            if (ivm != null)
+            {
+                ivm.PropertyChanged += ivm_PropertyChanged;
+            }
+            SetVisualState();
+	    }
+
+        void ivm_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            SetVisualState();
+        }
+
+	    private void SetVisualState()
+	    {
+	        var ivm = this.DataContext as IndicatorViewModel;
 	        if (ivm != null)
 	        {
+	            //ivm.PropertyChanged
 	            VisualStateManager.GoToState(this, ivm.IsValueHigherNormal == true ? "AlarmState" : "Default", true);
-                //http://lfhck.com/question/363851/binding-visualstatemanager-view-state-to-a-mvvm-viewmodel
+	            //http://lfhck.com/question/363851/binding-visualstatemanager-view-state-to-a-mvvm-viewmodel
 	        }
-            
 	    }
 	}
 }
