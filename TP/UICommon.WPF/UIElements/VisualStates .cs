79ef3fb11ef6201dbe73bec6c076af6bdcbadbbd
vovavo2006@gmail.com <vovano2006@gmail.com>
2014-02-15 18:35:47 +0000

    Добавлен класс VisualStates позволяющий контролировать анимацию состояний для визуальных анимированых контролов непосредствено из viewmodel

---
+++ TP/UICommon.WPF/UIElements/VisualStates .cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UICommon.WPF.UIElements
+{
+    public static class VisualStates
+    {
+        public static readonly DependencyProperty CurrentStateProperty =
+            DependencyProperty.RegisterAttached("CurrentState", typeof(String), typeof(VisualStates), new PropertyMetadata(TransitionToState));
+
+        public static string GetCurrentState(DependencyObject obj)
+        {
+            return (string)obj.GetValue(CurrentStateProperty);
+        }
+
+        public static void SetCurrentState(DependencyObject obj, string value)
+        {
+            obj.SetValue(CurrentStateProperty, value);
+        }
+
+        private static void TransitionToState(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            Control c = sender as Control;
+            if (c != null)
+            {
+                VisualStateManager.GoToState(c, (string)args.NewValue, true);
+            }
+            else
+            {
+                throw new ArgumentException("CurrentState is only supported on the Control type");
+            }
+        }
+    }
+}
