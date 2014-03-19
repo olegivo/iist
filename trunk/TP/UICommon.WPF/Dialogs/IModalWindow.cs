5cb68f8d422387530faae229e19cb60dbad02257
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2014-03-19 04:30:07 +0000

    UICommon.WPF: добавлен референс на Autofac добавлен сервис диалогов

---
+++ trunk/TP/UICommon.WPF/Dialogs/IModalWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UICommon.WPF.Dialogs
+{
+    public interface IModalWindow<TViewModel>
+    {
+        bool? DialogResult { get; set; }
+        event EventHandler Closed;
+        void Show();
+        bool? ShowDialog();
+        TViewModel ViewModel { get; set; }
+        string Title { get; set; }
+        void Close();
+    }
+}
