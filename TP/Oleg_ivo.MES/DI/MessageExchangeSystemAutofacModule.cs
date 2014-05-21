a2daf612b563d07d64b535ad442dd3c559332ea1
olegivo@gmail.com <vovano2006@gmail.com>
2014-05-21 10:36:24 +0000

    Запись данных, пришедших из канала в базу через DataContext

---
+++ TP/Oleg_ivo.MES/DI/MessageExchangeSystemAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Oleg_ivo.Base.Autofac.Modules;
+using Oleg_ivo.Plc.Entities;
+
+namespace Oleg_ivo.MES.DI
+{
+    public class MessageExchangeSystemAutofacModule : BaseAutofacModule
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterType<PlcDataContext>().SingleInstance();//TODO: регистрация конструктора в контексте
+        }
+    }
+}
