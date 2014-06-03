23bbd4db0ae35540e01c5bd52f42c1d35cbe2577
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2014-06-03 04:47:39 +0000

    Видимость каналов для клиентов верхнего уровня Issue:

---
+++ trunk/TP/PlcDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_LogicalChannelClient_LogicalChannel.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[LogicalChannelClient]
+    ADD CONSTRAINT [FK_LogicalChannelClient_LogicalChannel] FOREIGN KEY ([LogicalChannnelId]) REFERENCES [dbo].[LogicalChannel] ([Id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+
--- trunk/TP/PlcDatabase/Schema Objects/Schemas/dbo/Tables/LogicalChannelClient.table.sql
+++ trunk/TP/PlcDatabase/Schema Objects/Schemas/dbo/Tables/LogicalChannelClient.table.sql
@@ -1,6 +1,6 @@
-﻿/*CREATE TABLE [dbo].[ClientLogicalChannel] (
+﻿CREATE TABLE [dbo].[LogicalChannelClient] (
     [Id]                INT IDENTITY (1, 1) NOT NULL,
     [ClientId]          INT NOT NULL,
     [LogicalChannnelId] INT NOT NULL
-);*/
+);
 
