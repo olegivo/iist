a0df2a57ba5f6e8e8c11bb97ea74121657d1c948
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2014-07-09 19:30:05 +0000

    Протоколирование событий в БД

--- trunk/TP/DMS.Common/Messages/InternalServiceMessage.cs
+++ trunk/TP/DMS.Common/Messages/InternalServiceMessage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.Serialization;
+using System.Text;
 
 namespace DMS.Common.Messages
 {
@@ -24,5 +25,32 @@ namespace DMS.Common.Messages
 
         [DataMember]
         public abstract EventType EventType { get; }
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            if (!string.IsNullOrEmpty(RegNameFrom)) sb.AppendFormat(" from [{0}]", RegNameFrom);
+            if (!string.IsNullOrEmpty(RegNameTo)) sb.AppendFormat(" to [{0}]", RegNameTo);
+            if (sb.Length == 0) sb.AppendFormat(" ({0})", GetMessageType());
+            sb.Append(GetMessageDescription());
+            sb.Insert(0, "Message");
+            return sb.ToString();
+        }
+
+        protected virtual string GetMessageType()
+        {
+            return GetType().Name;
+        }
+
+        protected virtual string GetMessageDescription()
+        {
+            return "";
+        }
     }
 }
