d1bf1b0252cad22bbc98d01627d4efa7f6b78971
olegivo@gmail.com <olegivo@gmail.com@cd4ccbc2-a52b-cba9-5022-28d96650ba7f>
2014-07-09 18:27:09 +0000

    Таблицы для протоколирования событий

---
+++ trunk/TP/PlcDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_ProtocolEvent_Client.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[ProtocolEvent]
+    ADD CONSTRAINT [FK_ProtocolEvent_Client] FOREIGN KEY ([ClientId]) REFERENCES [dbo].[Client] ([ClientId]) ON DELETE CASCADE ON UPDATE CASCADE;
+
---
+++ trunk/TP/PlcDatabase/Schema Objects/Schemas/dbo/Tables/Keys/FK_ProtocolEvent_LogicalChannel.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[ProtocolEvent]
+    ADD CONSTRAINT [FK_ProtocolEvent_LogicalChannel] FOREIGN KEY ([LogicalChannelId]) REFERENCES [dbo].[LogicalChannel] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE;
+
